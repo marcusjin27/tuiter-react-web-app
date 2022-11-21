277f3708820e80604ca0dfe66c8351faf9b51959
Your Name <you@example.com>
2022-11-20 20:59:23 -0500

    a8 REST

--- src/tuiter/post-list/post-item.js
+++ src/tuiter/post-list/post-item.js
@@ -1,20 +1,20 @@
 import React from "react";
 import TuitStats from "./tuit-stats";
-import {useDispatch} from "react-redux";
-import {deleteTuit} from "../reducers/post-item-reducer"
+import { useDispatch } from "react-redux";
+import { deleteTuitThunk } from "../../services/tuits-thunks";
 
 const PostItem = (
     {
         post = {
-            _id: 2345,
+            _id: 234,
             topic: "Space",
             userName: "SpaceX",
             time: "2h",
             title: "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
             image: "spacex.png",
             liked: true,
-            replies: 189,
-            retuits: 4323,
+            replies: 123,
+            retuits: 432,
             likes: 2345,
             handle: "@spacex",
             tuit: "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
@@ -23,33 +23,33 @@ const PostItem = (
 ) => {
     const dispatch = useDispatch();
     const deleteTuitHandler = (id) => {
-        dispatch(deleteTuit(id));
+        dispatch(deleteTuitThunk(id));
     }
 
-    return(
+    return (
         <li className="list-group-item m-0 pt-2 pb-3 ps-1 pe-3">
             <div className="row">
                 <div className="col-2 justify-content-center pe-0">
-                    <img alt="text" width={70} className="rounded-circle p-2" src={`/images/${post.image}`}/>
+                    <img alt="text" width={70} className="rounded-circle p-2" src={`/images/${post.image}`} />
                 </div>
                 <div className="col-10 ps-0">
                     <div className="row pb-1">
                         <div className="col-11">
                             <span className="wd-font-size-medium wd-font-color-black wd-bold">{post.userName}</span>
-                            <i className="bi bi-patch-check-fill text-primary ms-1 me-1"/>
+                            <i className="bi bi-patch-check-fill text-primary ms-1 me-1" />
                             <span className="wd-font-size-medium wd-font-color-grey">@{post.handle}</span>
                             <span className="wd-font-size-medium wd-font-color-grey ps-1 pe-1">·</span>
                             <span className="wd-font-size-medium wd-font-color-grey">{post.time}</span>
                         </div>
                         <div className="col-1">
-                            <i className="bi bi-x-lg float-end" onClick={() => deleteTuitHandler(post._id)}/>
+                            <i className="bi bi-x-lg float-end" onClick={() => deleteTuitHandler(post._id)} />
                         </div>
                     </div>
                     <div>
                         <span className="wd-font-size-medium wd-font-color-black">{post.tuit}</span>
                     </div>
                     <div className="row pt-3 ps-1">
-                        <TuitStats post={post}/>
+                        <TuitStats post={post} />
                     </div>
                 </div>
             </div>
--- src/tuiter/post-list/tuit-stats.js
+++ src/tuiter/post-list/tuit-stats.js
@@ -1,34 +1,61 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { updateTuitThunk } from "../../services/tuits-thunks";
+
 
 const TuitStats = (
     {
         post = {
             liked: true,
-            replies: 222,
-            retuits: 4325,
-            likes: 365226,
+            replies: 123,
+            retuits: 432,
+            likes: 2345,
         }
     }
 ) => {
-    return(
+    const dispatch = useDispatch();
+
+    return (
         <>
             <div className="col-3 wd-font-size-medium-small wd-font-color-grey">
-                <i className="bi bi-chat"/>
+                <i className="bi bi-chat" />
                 <span className="ps-3">{post.replies}</span>
             </div>
             <div className="col-3 wd-font-size-medium-small wd-font-color-grey">
-                <i className="bi bi-repeat"/>
+                <i className="bi bi-repeat" />
                 <span className="ps-3">{post.retuits}</span>
             </div>
             <div className="col-3 wd-font-size-medium-small wd-font-color-grey">
-                {post.liked && (<i className="bi bi-heart-fill wd-red"/>)}
-                {!post.liked && (<i className="bi bi-heart"/>)}
+                {post.liked && (<i onClick={() => dispatch(updateTuitThunk({
+                    ...post,
+                    likes: post.likes - 1,
+                    liked: false,
+                }))} className="bi bi-heart-fill wd-red" />)}
+                {!post.liked && (<i onClick={() => dispatch(updateTuitThunk({
+                    ...post,
+                    likes: post.likes + 1,
+                    liked: true,
+                }))} className="bi bi-heart" />)}
                 <span className="ps-3">{post.likes}</span>
             </div>
-            <div className="col-3 wd-font-size-medium-small wd-font-color-grey">
-                <i className="bi bi-box-arrow-up"/>
+            <div className="col-2 wd-font-size-medium-small wd-font-color-grey">
+                {post.disliked && (<i onClick={() => dispatch(updateTuitThunk({
+                    ...post,
+                    dislikes: post.dislikes - 1,
+                    disliked: false,
+                }))} className="bi bi-hand-thumbs-down-fill wd-green" />)}
+                {!post.disliked && (<i onClick={() => dispatch(updateTuitThunk({
+                    ...post,
+                    dislikes: post.dislikes + 1,
+                    disliked: true,
+                }))} className="bi bi-hand-thumbs-down-fill" />)}
+                <span className="ps-3">{post.dislikes}</span>
+            </div>
+            <div className="col-1 wd-font-size-medium-small wd-font-color-grey">
+                <i className="bi bi-box-arrow-up" />
             </div>
         </>
     );
 };
+
 export default TuitStats;
